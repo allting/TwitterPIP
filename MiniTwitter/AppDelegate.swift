f0953aba0457be98555aede0bddb5f71a25c71ff
krkim <allting@gmail.com>
2017-03-25 15:11:23 +0900

    Initial Commit

---
+++ MiniTwitter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MiniTwitter
+//
+//  Created by kkr on 25/03/2017.
+//  Copyright © 2017 allting. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MiniTwitter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MiniTwitter
+//
+//  Created by kkr on 25/03/2017.
+//  Copyright © 2017 allting. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
